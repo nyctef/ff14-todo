a0d9f39ffa039e81acfc96faff595c846ab28c12
Nyctef <nyctef@nyctef.com>
2024-06-01 20:32:48 +0100

    first sketch of setting a todo via api

--- srv/index.ts
+++ srv/index.ts
@@ -3,9 +3,25 @@ import { todos } from "./api";
 
 const app = express();
 app.use(express.static("../web/dist"));
+app.use(express.json());
 
 app.get("/api/todos", (req: Request, res: Response) => {
+  console.log("GET /api/todos");
   res.json(todos);
+});
+
+app.post("/api/todos/:id/completed", (req: Request, res: Response) => {
+  console.log("POST /api/todos/:id/completed");
+  const id = parseInt(req.params.id, 10);
+  const completed = req.body.completed;
+
+  const todo = todos.find((todo) => todo.id === id);
+  if (!todo) {
+    return res.sendStatus(404);
+  }
+  todo.lastDone = completed ? new Date() : null;
+
+  res.sendStatus(204);
 });
 
 const port = process.env.PORT || 3000;
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,67 @@
-import type { Component } from 'solid-js';
-import Comp from './Comp';
+import { For, createResource, type Component } from "solid-js";
+import { Todo } from "../../share/types";
+
+const TodoCheckbox: Component<{
+  todo: Todo;
+  setCompleted: (value: boolean) => void;
+}> = (props) => {
+  const completed = () =>
+    props.todo.lastDone && props.todo.lastDone <= new Date();
+
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={completed()}
+        onChange={(e) => {
+          // doesn't seem like `e.preventDefault()` or `return false` works here
+          // need to immediately reset the checkbox to its previous state
+          e.target.checked = completed();
+          props.setCompleted(!completed());
+        }}
+      />
+      {props.todo.text}
+    </label>
+  );
+};
 
 const App: Component = () => {
+  const [todos, { refetch }] = createResource(async () => {
+    const response = (await (await fetch("/api/todos")).json()) as Todo[];
+    const todos = response.map((todo) => ({
+      ...todo,
+      lastDone: todo.lastDone ? new Date(todo.lastDone) : null,
+    }));
+    console.log({ todos });
+    return todos;
+  });
+
+  async function setTodoCompleted(id: number, value: boolean) {
+    console.log(`TODO: set ${id} ${value}`);
+    await fetch(`/api/todos/${id}/completed`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ completed: value }),
+    });
+    // TODO: avoid double roundtrip to api
+    refetch();
+  }
+
   return (
     <>
-      <h1>Hello world!!!!</h1>
-      <Comp />
+      <h1>FF14 TODOs</h1>
+      <ul>
+        <For each={todos()} fallback={<li>Loading...</li>}>
+          {(todo) => (
+            <li>
+              <TodoCheckbox
+                todo={todo}
+                setCompleted={(x) => setTodoCompleted(todo.id, x)}
+              />
+            </li>
+          )}
+        </For>
+      </ul>
     </>
   );
 };
