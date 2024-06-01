cb508ec2a3fd89f3cbe9e556ca4ee463ac7b3f9b
Nyctef <nyctef@nyctef.com>
2024-06-01 20:49:57 +0100

    don't refetch todos after setting completed state
    this does mean we need to duplicate more logic between the front and backend, though :/

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-import { For, createResource, type Component } from "solid-js";
+import { For, createResource, onMount, type Component } from "solid-js";
+import { createStore } from "solid-js/store";
 import { Todo } from "../../share/types";
 
 const TodoCheckbox: Component<{
   todo: Todo;
-  setCompleted: (value: boolean) => void;
+  setCompleted: (id: number, lastDone: Date | null) => void;
 }> = (props) => {
   const completed = () =>
     props.todo.lastDone && props.todo.lastDone <= new Date();
@@ -14,10 +15,11 @@ const TodoCheckbox: Component<{
         type="checkbox"
         checked={completed()}
         onChange={(e) => {
+          const wasCompleted = completed();
           // doesn't seem like `e.preventDefault()` or `return false` works here
           // need to immediately reset the checkbox to its previous state
-          e.target.checked = completed();
-          props.setCompleted(!completed());
+          e.target.checked = wasCompleted;
+          props.setCompleted(props.todo.id, wasCompleted ? null : new Date());
         }}
       />
       {props.todo.text}
@@ -26,38 +28,37 @@ const TodoCheckbox: Component<{
 };
 
 const App: Component = () => {
-  const [todos, { refetch }] = createResource(async () => {
+  const [todos, setTodos] = createStore<Todo[]>([]);
+  onMount(async () => {
     const response = (await (await fetch("/api/todos")).json()) as Todo[];
     const todos = response.map((todo) => ({
       ...todo,
+      // dates will be serialized in json as strings,
+      // so we need to convert them back
       lastDone: todo.lastDone ? new Date(todo.lastDone) : null,
     }));
     console.log({ todos });
-    return todos;
+    setTodos(todos);
   });
 
-  async function setTodoCompleted(id: number, value: boolean) {
+  async function setTodoCompleted(id: number, value: Date | null) {
     console.log(`TODO: set ${id} ${value}`);
     await fetch(`/api/todos/${id}/completed`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ completed: value }),
     });
-    // TODO: avoid double roundtrip to api
-    refetch();
+    setTodos((t) => t.id === id, { lastDone: value });
   }
 
   return (
     <>
       <h1>FF14 TODOs</h1>
       <ul>
-        <For each={todos()} fallback={<li>Loading...</li>}>
+        <For each={todos} fallback={<li>Loading...</li>}>
           {(todo) => (
             <li>
-              <TodoCheckbox
-                todo={todo}
-                setCompleted={(x) => setTodoCompleted(todo.id, x)}
-              />
+              <TodoCheckbox todo={todo} setCompleted={setTodoCompleted} />
             </li>
           )}
         </For>
